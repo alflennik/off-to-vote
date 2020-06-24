5240cbcf934e26474a9a7a02423e0b5cfbf0b982
Alex Flenniken <alex.flenniken@whiteops.com>
2020-06-23 23:33:42 -0400

    Fix function

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,7 @@
-// const functions = require('firebase-functions');
-// const signPledge = require('./signPledge')
+const functions = require('firebase-functions');
+const signPledge = require('./signPledge')
 
-// exports.signPledge = functions.https.onRequest((req, res) => {
-//   console.log('serving')
-//   signPledge(req, res)
-// });
-
-const functions = require('firebase-functions')
-// const moment = require('moment-timezone')
-
-const serve = (req, res) => {
-  res.send('success')
-}
-
-exports.serve = functions.https.onRequest(async (req, res) => {
-  await serve(req, res)
-})
+exports.signPledge = functions.https.onRequest((req, res) => {
+  console.log('serving')
+  signPledge(req, res)
+});
