1eaa3ab8665c81f2e1ca781800da2086a4fa0feb
Alex Flenniken <alex.flenniken@whiteops.com>
2020-06-27 21:48:51 -0400

    Delete function

--- functions/index.js
+++
@@ -1,6 +0,0 @@
-const functions = require('firebase-functions');
-const signPledge = require('./signPledge')
-
-exports.signPledge = functions.https.onRequest((req, res) => {
-  signPledge(req, res)
-});
